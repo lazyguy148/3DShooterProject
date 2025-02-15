bb5f0039ef7711028236e2d82033ea07a60bd4fe
lazyguy148 <thuatlongan123@gmail.com>
2025-02-16 01:27:54 +0700

    Enemy Health

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] int damageAmount = 1;
+
     StarterAssetsInputs starterAssetsInputs;
 
     void Awake()
@@ -12,15 +14,21 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
-        if (starterAssetsInputs.shoot)
-        {
-            RaycastHit hit;
+        HandleShoot();
 
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
-            {
-                Debug.Log(hit.collider.name);
-                starterAssetsInputs.ShootInput(false);
-            }
+    }
+
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damageAmount);
+            starterAssetsInputs.ShootInput(false);
         }
     }
 }
