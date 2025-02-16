1d3d8c0d0d046964f75aa1f10e1634f8bfe617d0
lazyguy148 <thuatlongan123@gmail.com>
2025-02-17 00:08:09 +0700

    Shoot Cooldown

---
+++ Assets/Scripts/ActiveWeapon.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using StarterAssets;
+
+public class ActiveWeapon : MonoBehaviour
+{
+    [SerializeField] WeaponSO weaponSO;
+    Animator animator;
+
+    StarterAssetsInputs starterAssetsInputs;
+    Weapon currentWeapon;
+
+    const string SHOOT_STRING = "Shoot";
+
+    float TimeSinceLastShot = 0f;
+
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        currentWeapon = GetComponentInChildren<Weapon>();
+    }
+
+    void Update()
+    {
+        TimeSinceLastShot += Time.deltaTime;
+        HandleShoot();
+    }
+
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+        if(TimeSinceLastShot >= weaponSO.FireRate)
+        {
+            currentWeapon.Shoot(weaponSO);
+            animator.Play(SHOOT_STRING, 0, 0f);
+            TimeSinceLastShot = 0f;
+        }
+        starterAssetsInputs.ShootInput(false);
+
+        
+    }
+}
