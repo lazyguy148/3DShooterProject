f7335a7845e036b738ef6606a5b188c6b4f9c445
Hieu Nguyen <anhhieunguyen2507@gmail.com>
2025-02-19 23:32:00 +0700

    Raycast Overloads

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,12 +7,13 @@ public class Weapon : MonoBehaviour
     [SerializeField] GameObject bulletHolePrefab;
     [SerializeField] float bulletHoleLifetime = 5f;
     [SerializeField] LayerMask hitLayers;
+    [SerializeField] LayerMask interactionLayers;
     public void Shoot(WeaponSO weaponSO)
     {
         muzzleFlash.Play();
         RaycastHit hit;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, interactionLayers, QueryTriggerInteraction.Ignore))
         {
             GameObject vfx = Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
