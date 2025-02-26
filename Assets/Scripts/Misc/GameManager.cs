fd83e290ca0708804abee7dc8364a2e42235765a
Hieu Nguyen <anhhieunguyen2507@gmail.com>
2025-02-26 23:49:54 +0700

    Win Condition

--- Assets/Scripts/Misc/GameManager.cs
+++ Assets/Scripts/Misc/GameManager.cs
@@ -1,8 +1,27 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text enemiesLeftText;
+    [SerializeField] GameObject youWinText;
+
+    int enemiesLeft = 0;
+
+    const string ENEMIES_LEFT_STRING = "Enemies Left: ";
+
+    public void AdjustEnemiesLeft(int amount)
+    {
+        enemiesLeft += amount;
+        enemiesLeftText.text = ENEMIES_LEFT_STRING + enemiesLeft.ToString();
+
+        if(enemiesLeft <= 0)
+        {
+            youWinText.SetActive(true);
+        }
+    }
+
    public void RestartlevelButton()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
